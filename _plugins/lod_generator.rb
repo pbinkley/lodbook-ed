2d9e876497be818f5482485cedd5524762921b58
Peter Binkley <peter.binkley@ualberta.ca>
2017-09-05 18:45:02 -0600

    Add hyphen to regex for link anchors, so that slugified links won't be caught

--- _plugins/lod_generator.rb
+++ _plugins/lod_generator.rb
@@ -338,7 +338,7 @@ module Jekyll
           if para.inner_html =~ /\b#{label}\b/
             details = references[label]
             link = lod_link(label, details[:name])
-            para.inner_html = para.inner_html.gsub(/(?<!\>)(?<!\=")(?<!\-")\b#{label}\b(?!\<)(?!\-)(?!")/, link)
+            para.inner_html = para.inner_html.gsub(/(?<!\>)(?<!\=")(?<!\-)(?<!\/)\b#{label}\b(?!\<)(?!")(?!\-)(?!\/)/, link)
           end
         end
       end
